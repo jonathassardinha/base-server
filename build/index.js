59797785b73ef91fe1d57cdb1e0ca1f059fbaaba
jonathassardinha <jonathas.sardinha@gmail.com>
2021-03-11 17:07:22 -0300

    Removing build folder

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ioc_container_1 = require("./src/ioc/ioc-container");
-var container = ioc_container_1.createContainer();
-container.get('workers');
--- build/src/webcontrollers/basic-controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var BasicController = /** @class */ (function () {
-    function BasicController(router, baseRoute) {
-        router.get("/" + baseRoute, function (req, res) { return res.end('Route up'); });
-    }
-    return BasicController;
-}());
-exports.default = BasicController;
